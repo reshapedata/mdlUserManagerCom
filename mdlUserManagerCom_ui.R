319dd48d14bbeeb873be9dfdb968f91aca754b51
takewiki <hulilei@takewiki.com.cn>
2022-05-09 23:01:20 +0800

    v1.03

--- mdlUserManagerCom_ui.R
+++ mdlUserManagerCom_ui.R
@@ -3,7 +3,13 @@ tabItem(tabName = "mdlUserManagerCom",
                       column(width = 12,
                              tabBox(title ="用户管理管理工作台",width = 12,
                                     id='tabSet_mdlMultipleMaterialCom',height = '300px',
-                                    mdlUserManagerUI::userAddUI()
+                                    mdlUserManagerUI::userAddUI(),
+                                    mdlUserManagerUI::userQueryUI(),
+                                    mdlUserManagerUI::userRoleSettingUI(),
+                                    mdlUserManagerUI::roleAddUI(),
+                                    mdlUserManagerUI::roleQueryUI(),
+                                    mdlUserManagerUI::roleRightUI()
+                                    
                                    
                                     
                                     
